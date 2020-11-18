3f64d2e391e4550831dbb87ff388035b6b3e023f
rajat <rajatsharma1492@gmail.com>
2020-11-18 16:54:20 +0530

    Minor changes done

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 let express = require('express');
 let app = express();
 
-let bodyParser = require('body-parser');
-let port = 8080;
+// let bodyParser = require('body-parser');
+let port = process.env.PORT;
 
-//parse application/json and look for raw text                                        
-app.use(bodyParser.json());                                     
-app.use(bodyParser.urlencoded({extended: true}));               
-app.use(bodyParser.text());                                    
-app.use(bodyParser.json({ type: 'application/json'}));  
+// //parse application/json and look for raw text                                        
+// app.use(bodyParser.json());                                     
+// app.use(bodyParser.urlencoded({extended: true}));               
+// app.use(bodyParser.text());                                    
+// app.use(bodyParser.json({ type: 'application/json'}));  
 
 app.get("/", (req, res) => res.json({message: "Welcome to our Bookstore!"}));
 
